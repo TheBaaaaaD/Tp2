3ea854647dae6303e7a08129f41840a5f1f037e6
TheBaaaaaD <62421223+TheBaaaaaD@users.noreply.github.com>
2020-03-23 15:21:34 +0100

    Create Compteur.h

---
+++ Compteur.h
@@ -0,0 +1,36 @@
+#ifndef DEF_Compteur // Si la constante n'a pas été définie le fichier n'a jamais été inclus
+#define DEF_Compteur // On définit la constante pour que la prochaine fois le fichier ne soit plus inclus
+
+
+    #include <iostream>
+    #include <fstream>
+    #include <string>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <stdio.h>
+    
+    
+using namespace std;
+  
+//Classes
+
+class Compt{
+    private :
+        int compteur;
+        int modulo;
+    public :
+        Compt(int,int);
+        void incrementer(); 
+        void incrementerN(int);
+        int getEtat();
+    
+};
+  
+  
+    
+//Prototypes
+
+//Fonctions
+
+
+#endif
