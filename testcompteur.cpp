91205bc227e6f7ffb338bca34b647c33137976d6
TheBaaaaaD <62421223+TheBaaaaaD@users.noreply.github.com>
2020-03-30 10:44:16 +0200

    Update testcompteur.cpp

--- testcompteur.cpp
+++ testcompteur.cpp
@@ -1,25 +1,39 @@
 #include "Compteur.h"
 
-
-Compt::Compt (int compt, int modul)	//fonction de crC)ation du constructeur
+int main ()
 {
-  compteur = compt;		//on donne les valeurs C  compteur et modul
-  modulo = modul;
-}
+  int compt = 0, modul = 6, n = 0, choix, stop;
 
-void Compt::incrementer ()		//fonction pour ajouter 1 au compteur
-{
-  compteur = (compteur + 1) % modulo; 
-  cout<<"On a incrementer de 1"<<endl;
-}
+//Pointeurs
 
-void Compt::incrementerN (int n)	//fonction pour incrementer de N
-{
-  compteur = (compteur + n) % modulo;
-  cout<<"On a incrementer de "<<n<<endl;
-}
+  Compt c1 (compt, modul);	//crC)ation de l'objet c1 de class Compt
 
-int Compt::getEtat ()		//fonction pour obtenir la valeur du compteur
-{
-  cout << compteur << endl;
+  do				//on boucle pour pouvoir utiliser le programme plusieur fois
+    {
+      cout <<
+	"Que voullez vous faire ?\n1-Incrementer de 1\n2-Incrementer de N\n3-Voir le compteur"<< endl;
+      cin >> choix;		//on donne la valeur de notre choix
+
+      switch (choix)
+	{
+	case 1:
+	  c1.incrementer ();	//appel de la fonction incrementer
+	  break;
+
+	case 2:
+	  cout << "De combien voullez vous incrementer ?" << endl;
+	  cin >> n;
+	  c1.incrementerN (n);	//appel de la fonction incrementerN
+	  break;
+
+	case 3:
+	  cout<<"Le compteur vaut : ";	//appel de la fonction getEtat
+	  c1.getEtat();
+	  break;
+	}
+      cout << "Voullez vous continuer ?\n1-Oui\n0-Non" << endl;
+      cin >> stop;		//on donne une valeur C  stop
+    }
+  while (stop != 0);		//tant que stop est different de 0 on continu
+  return 0;
 }
